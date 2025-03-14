254b95817d8b8b86e5153a2a37a56e2ed4c4cd33
android <android@LABFAE.local>
2025-03-14 20:50:20 -0300

    SistemaPagamento
    Exercícios 2 Sistema De Pagamento de Estacionamento incompleto

---
+++ aula4/SistemaPagamento/src/main/java/com/mycompany/sistemapagamento/SistemaPagamento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemapagamento;
+
+/**
+ *
+ * @author android
+ */
+public class SistemaPagamento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

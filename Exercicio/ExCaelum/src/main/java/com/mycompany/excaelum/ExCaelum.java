f86c3ec32ddd2225bf1da66530b9b401030a1320
android <112909349+LuccaAstrini@users.noreply.github.com>
2025-05-14 22:24:34 -0300

    comentario

--- Exercicio/ExCaelum/src/main/java/com/mycompany/excaelum/ExCaelum.java
+++ Exercicio/ExCaelum/src/main/java/com/mycompany/excaelum/ExCaelum.java
@@ -5,6 +5,7 @@ public class ExCaelum {
     public static void main(String[] args) {
         String cpf = "123.456.789-09";
         
+        //se for alterar o adapter apenas trocar o ValidadorCPFAdapter para o atualizado
         ProtocoloDeValidacaoDeCPF validador = new ValidadorCPFAdapter();
         
         if(validador.isCPFValido(cpf)) {
